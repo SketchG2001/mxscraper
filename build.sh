32f98d5fdec4f364c66d112d321e46e58d200154
vikas gole <129413413+SketchG2001@users.noreply.github.com>
2025-04-27 16:13:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,22 +9,14 @@ source ./.venv/bin/activate
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-# Install necessary dependencies (Chrome, ffmpeg)
-apt-get update
-apt-get install -y \
-  libx11-dev \
-  libxkbfile-dev \
-  libsecret-1-dev \
-  libxrandr-dev \
-  libxtst-dev \
-  libappindicator3-dev \
-  libatk-bridge2.0-dev \
-  libgdk-pixbuf2.0-dev \
-  libnss3 \
-  libxcomposite-dev \
-  libxdamage-dev \
-  wget \
-  ffmpeg
+# Install necessary dependencies (Chrome, ffmpeg) in user space
+
+# Download and install ffmpeg
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz
+tar -xf ffmpeg-release-i686-static.tar.xz -C $HOME/ffmpeg
+
+# Set environment variable for ffmpeg binary path
+export PATH=$HOME/ffmpeg/ffmpeg-*/bin:$PATH
 
 # Install Google Chrome in user-space (without root privileges)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.tar.gz
