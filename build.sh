c7f022c79f45bb3cdd94960861b665dc9b0b8652
vikas gole <129413413+SketchG2001@users.noreply.github.com>
2025-04-27 16:48:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,9 +14,10 @@ pip install -r requirements.txt
 
 # Install necessary dependencies (Chrome, ffmpeg) in user space
 
-# Download and install ffmpeg
+# Download and install FFmpeg
 echo "Downloading and installing FFmpeg..."
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz
+mkdir -p $HOME/ffmpeg
 tar -xf ffmpeg-release-i686-static.tar.xz -C $HOME/ffmpeg
 
 # Set environment variable for ffmpeg binary path
@@ -25,19 +26,19 @@ export PATH=$HOME/ffmpeg/ffmpeg-*/bin:$PATH
 
 # Install Headless Chromium (No root required)
 echo "Downloading and installing Headless Chromium..."
-wget https://github.com/ellisonleao/chromix-too/releases/download/v1.0.1/chromium-linux-x64.tar.gz
+wget https://github.com/Zenika/alpine-chrome/releases/download/v1.0.0/alpine-chrome-x64.tar.gz
 
 # Extract the Chromium tarball to $HOME
 echo "Extracting Chromium..."
-tar -xvzf chromium-linux-x64.tar.gz -C $HOME/chromium
+tar -xvzf alpine-chrome-x64.tar.gz -C $HOME/chromium
 
 # Set environment variable for Chrome binary path
 echo "Setting up Chromium environment variable..."
-export PATH=$HOME/chromium/chromium-linux-x64:$PATH
+export PATH=$HOME/chromium/alpine-chrome-x64:$PATH
 
 # Ensure Chromium is in the path
 echo "Verifying Chromium installation..."
-if [ -f "$HOME/chromium/chromium-linux-x64/chrome" ]; then
+if [ -f "$HOME/chromium/alpine-chrome-x64/chrome" ]; then
     echo "Chromium is installed."
 else
     echo "Chromium installation failed!"
